e517b6ab19d9cc99e193bc8cd74a85e5542b30a9
Atrequu <fabiaurrea68@gmail.com>
2022-08-23 19:46:16 -0500

    update README.md and create method saludar() into HolaMundo.java

---
+++ intro-git/HolaMundo.java
@@ -0,0 +1,9 @@
+class HolaMundo{
+    public static void main(String[] arg){
+        saludar("Andrés");
+    }
+
+    public static void saludar(String nombre){
+        System.out.println("Hola "+nombre+"!");
+    }
+}
